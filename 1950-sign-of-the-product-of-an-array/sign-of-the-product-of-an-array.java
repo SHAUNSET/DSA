37bbac7891ac50cddf9cd0285890024c86b023aa
Shaunak Naik <shaunak206107@gmail.com>
2025-12-17 19:44:59 +0530

    Time: 1 ms (21.39%) | Memory: 45.3 MB (18.05%) - LeetSync

---
+++ 1950-sign-of-the-product-of-an-array/sign-of-the-product-of-an-array.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int arraySign(int[] nums) {
+        int prod = 1;
+
+        for(int i = 0 ; i < nums.length ; i++){
+            if(nums[i] > 0){
+                prod = prod * 1;
+            }
+            else if(nums[i] < 0){
+                prod = prod * (-1);
+            }
+            else{
+                prod = prod * 0;
+            }
+        }
+
+        return prod;
+    }
+}
