cd1f593b69cb62443e84eac7aee1cd3f07f77f8f
Shaunak Naik <shaunak206107@gmail.com>
2025-12-23 19:30:31 +0530

    Time: 34 ms (24.90%) | Memory: 54.3 MB (72.93%) - LeetSync

---
+++ 937-online-stock-span/online-stock-span.java
@@ -0,0 +1,21 @@
+class StockSpanner {
+
+    Stack<int[]> stack;
+
+    public StockSpanner() {
+        stack = new Stack<>();
+    }
+
+    public int next(int price) {
+
+        int span = 1;
+
+        while (!stack.isEmpty() && price >= stack.peek()[0]) {
+            span += stack.peek()[1];
+            stack.pop();
+        }
+
+        stack.push(new int[]{price, span});
+        return span;
+    }
+}
