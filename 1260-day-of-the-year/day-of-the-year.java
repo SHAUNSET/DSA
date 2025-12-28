b463cb3419731466f5f76b04b321fb33c872ea55
Shaunak Naik <shaunak206107@gmail.com>
2025-12-28 23:27:58 +0530

    Time: 10 ms (45.30%) | Memory: 46.8 MB (84.29%) - LeetSync

---
+++ 1260-day-of-the-year/day-of-the-year.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int dayOfYear(String date) {
+
+
+        String[] parts = date.split("-");
+        int year = Integer.parseInt(parts[0]);
+        int month = Integer.parseInt(parts[1]);
+        int day = Integer.parseInt(parts[2]);
+
+        int[] daysInMonth = {
+            31, 28, 31, 30, 31, 30,
+            31, 31, 30, 31, 30, 31
+        };
+
+        boolean isLeap = (year % 400 == 0) || 
+                         (year % 4 == 0 && year % 100 != 0);
+
+        if (isLeap) {
+            daysInMonth[1] = 29;
+        }
+
+
+        int totalDays = 0;
+        for (int i = 0; i < month - 1; i++) {
+            totalDays += daysInMonth[i];
+        }
+
+
+        totalDays += day;
+
+        return totalDays;
+    }
+}
