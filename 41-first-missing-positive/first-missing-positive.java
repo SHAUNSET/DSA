adebe4a4943228ced909cbcb16158caaa2806d0b
Shaunak Naik <shaunak206107@gmail.com>
2026-01-28 18:37:24 +0530

    Time: 17 ms (18.15%) | Memory: 74.1 MB (25.32%) - LeetSync

---
+++ 41-first-missing-positive/first-missing-positive.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+        Arrays.sort(nums);
+        int expected = 1;
+
+        for (int num : nums) {
+            if (num < expected) continue;
+            if (num == expected) expected++;
+            else break;
+        }
+        return expected;
+    }
+}
