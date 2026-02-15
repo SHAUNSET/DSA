b681977d9c43b4b3615dffcd0723671f7e0e34a7
Shaunak Naik <shaunak206107@gmail.com>
2026-02-15 20:07:06 +0530

    Time: 2 ms (60.54%) | Memory: 46.2 MB (90.60%) - LeetSync

---
+++ 383-ransom-note/ransom-note.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        int[] count = new int[26];
+        
+        for (char c : ransomNote.toCharArray()) {
+            count[c - 'a']++;
+        }
+        
+        for (char c : magazine.toCharArray()) {
+            if (count[c - 'a'] > 0) {
+                count[c - 'a']--;
+            }
+        }
+        
+        for (int i = 0; i < 26; i++) {
+            if (count[i] > 0) return false;
+        }
+        
+        return true;
+    }
+}
