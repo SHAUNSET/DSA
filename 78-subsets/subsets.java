f4886b3df00b03bb33e0a577afea7b0c395ac5b3
Shaunak Naik <shaunak206107@gmail.com>
2026-03-25 16:20:48 +0530

    Time: 1 ms (88.41%) | Memory: 44.2 MB (69.49%) - LeetSync

---
+++ 78-subsets/subsets.java
@@ -0,0 +1,26 @@
+class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+
+        List<List<Integer>> result = new ArrayList<>();
+        List<Integer> current = new ArrayList<>();
+
+        solve(0, nums, current, result);
+
+        return result;
+    }
+
+    void solve(int index, int[] nums, List<Integer> current, List<List<Integer>> result) {
+
+        if (index == nums.length) {
+            result.add(new ArrayList<>(current));
+            return;
+        }
+
+        current.add(nums[index]);
+        solve(index + 1, nums, current, result);
+
+        current.remove(current.size() - 1);
+
+        solve(index + 1, nums, current, result);
+    }
+}
