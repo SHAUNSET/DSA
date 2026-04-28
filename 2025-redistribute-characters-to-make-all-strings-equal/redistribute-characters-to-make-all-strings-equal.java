bc167bd42c8364ed865151782cb2e4d8aeec844c
Shaunak Naik <shaunak206107@gmail.com>
2026-04-28 20:28:34 +0530

    Time: 11 ms (27.04%) | Memory: 46.1 MB (24.06%) - LeetSync

---
+++ 2025-redistribute-characters-to-make-all-strings-equal/redistribute-characters-to-make-all-strings-equal.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean makeEqual(String[] words) {
+        HashMap<Character, Integer> map = new HashMap<>();
+
+
+        for (String word : words) {
+            for (char c : word.toCharArray()) {
+                map.put(c, map.getOrDefault(c, 0) + 1);
+            }
+        }
+
+        int n = words.length;
+
+        for (int freq : map.values()) {
+            if (freq % n != 0) return false;
+        }
+
+        return true;
+    }
+}
