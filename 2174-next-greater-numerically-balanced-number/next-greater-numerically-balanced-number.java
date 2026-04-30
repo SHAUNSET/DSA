2cabc392e0bcba8b93eb161c20ec3e6385c09763
Shaunak Naik <shaunak206107@gmail.com>
2026-05-01 00:06:33 +0530

    Time: 90 ms (64.59%) | Memory: 45.8 MB (77.22%) - LeetSync

---
+++ 2174-next-greater-numerically-balanced-number/next-greater-numerically-balanced-number.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int nextBeautifulNumber(int n) {
+        int num = n + 1;
+
+        while (true) {
+            if (isBalanced(num)) {
+                return num;
+            }
+            num++;
+        }
+    }
+
+    private boolean isBalanced(int num) {
+        int[] freq = new int[10];
+
+        int temp = num;
+        while (temp > 0) {
+            int digit = temp % 10;
+            freq[digit]++;
+            temp /= 10;
+        }
+
+        for (int d = 0; d <= 9; d++) {
+            if (freq[d] != 0 && freq[d] != d) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
