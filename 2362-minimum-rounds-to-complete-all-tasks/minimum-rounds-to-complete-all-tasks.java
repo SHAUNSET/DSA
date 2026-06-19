ff3993dc6e02138da8454fe8dca45581258905e0
Shaunak Naik <shaunak206107@gmail.com>
2026-06-19 21:51:22 +0530

    Time: 30 ms (83.59%) | Memory: 108.2 MB (61.03%) - LeetSync

---
+++ 2362-minimum-rounds-to-complete-all-tasks/minimum-rounds-to-complete-all-tasks.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int minimumRounds(int[] tasks) {
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int t : tasks) {
+            map.put(t, map.getOrDefault(t, 0) + 1);
+        }
+
+        int rounds = 0;
+
+        for (int freq : map.values()) {
+
+            if (freq == 1) return -1;
+
+            if (freq % 3 == 0) {
+                rounds += freq / 3;
+            } 
+            else if (freq % 3 == 1) {
+                rounds += (freq / 3 - 1) + 2;
+            } 
+            else {
+                rounds += (freq / 3) + 1;
+            }
+        }
+
+        return rounds;
+    }
+}
